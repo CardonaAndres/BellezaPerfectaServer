de3896a7dfd1b1e13c656d4c4868e2a1c4232c12
Andrés Cardona <135064128+Totoy777@users.noreply.github.com>
2025-05-05 14:35:35 -0500

    fix: Corregir la configuración del host de la base de datos y asegurar la conversión de cantidad a entero en la actualización de stock

--- src/invoices/invoices.service.ts
+++ src/invoices/invoices.service.ts
@@ -342,7 +342,7 @@ export class InvoicesService {
  
     for (const detail of invoice.details) {
       await this.productService.update(detail.product_ID.product_ID, {
-        stock: detail.product_ID.stock + detail.quantity,
+        stock: detail.product_ID.stock + parseInt(String(detail.quantity)),
       }, 'Factura', 'Eliminacion de la factura: ' + invoice_ID);
     }
 
