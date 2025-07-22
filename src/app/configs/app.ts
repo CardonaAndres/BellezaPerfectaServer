9ba64c3f771aa798af9cc14470b90a324f51e61c
Andrés Cardona <135064128+Totoy777@users.noreply.github.com>
2025-07-22 00:19:21 -0500

    fix: actualizar configuración de CLIENTS y SECRET_KEY para mayor seguridad y funcionalidad

--- src/app/configs/app.ts
+++ src/app/configs/app.ts
@@ -1,13 +1,17 @@
 export const PORT = process.env.PORT ?? 3000;
-export const CLIENTS = ['http://localhost:5173'];
+export const CLIENTS = [
+  'https://belleza-perfecta.com',
+  'https://belleza-perfecta.vercel.app'
+];
+
 export const SECRET_KEY = process.env.SECRET_KEY || '';
 
 export const database_credentials = {
   type : 'mysql' as const,
-  host : 'host.docker.internal' as const, // dokcer(host.docker.internal) | trabajo en local(localhost)
+  host : 'mysql' as const, // dokcer(host.docker.internal) | trabajo en local(localhost)
   port: 3306 as const,
-  username: 'root' as const,
-  password: '' as const,
+  username: 'bellezaperfecta_user' as const,
+  password: 'bellezaperfecta_pass' as const,
   database: 'bellezaperfecta_db' as const,
 }
 
